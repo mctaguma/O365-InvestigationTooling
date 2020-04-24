0ab29af4dc29279a179a6c807966a120b8152695
Matthew McTague <matt@mctague.nz>
2020-04-24 13:47:03 +1200

    Update EnableMailboxAuditing.ps1

--- EnableMailboxAuditing.ps1
+++ EnableMailboxAuditing.ps1
@@ -1,4 +1,9 @@
-# Connect using Exchange Online PowerShell module (Connect-EXOPSSession)
+# Refer to https://docs.microsoft.com/en-us/powershell/exchange/exchange-online/exchange-online-powershell-v2/exchange-online-powershell-v2?view=exchange-ps
+# Refer to https://www.powershellgallery.com/packages/ExchangeOnlineManagement/0.4578.0
+# Run the following in an administrative PowerShell session, to install the Exchange Online PowerShell module: Install-Module ExchangeOnlineManagement; Update-Module ExchangeOnlineManagement
+# This script can be run in a limited user PowerShell session
+
+Connect-ExchangeOnline
 
 #Enable global audit logging
 foreach ($mailbox in Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox" -or RecipientTypeDetails -eq "SharedMailbox" -or RecipientTypeDetails -eq "RoomMailbox" -or RecipientTypeDetails -eq "DiscoveryMailbox"})
